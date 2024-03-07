ca0c58350c478af3ae577bd7e722118ec64c37ad
Islam Ahmed <eslamelbltagye1111@gmail.com>
2024-03-08 00:48:30 +0200

    a

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,6 +7,7 @@ of the web_static folder of your AirBnB Clone repo, using the function do_pack.
 from fabric.api import local
 from datetime import datetime
 
+
 def do_pack():
     """archive file"""
 
@@ -15,7 +16,10 @@ def do_pack():
         return None
 
     time = datetime.utcnow()
-    arc_file = 'versions/web_static_{}{}{}{}{}.tgz'.format(time.year, time.month, time.day, time.hour, time.minute, time.second)
+
+    arc_file = 'versions/web_static_{}{}{}{}{}.tgz'
+    .format(time.year, time.month, time.day,
+            time.hour, time.minute, time.second)
 
     compress = local("tar -czf {} web_static".format(arc_file))
 
